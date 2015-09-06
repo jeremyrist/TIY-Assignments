fb7cff35c335ba3064fc63a51207bd613ff5781b
Jeremy Rist <jtrain18@brandeis.edu>
2015-09-06 16:53:01 -0400

    Update EvenFibonaccis.js

--- EvenFibonaccis.js
+++ EvenFibonaccis.js
@@ -1,1 +1,36 @@
 
+var text = require('mocha').it,
+  expects = require('chai').expect;
+
+test('generating the Fibonacci sequence', function(){
+  expect(fibonacci(0)).to.deep.equal([ ]);
+  expect(fibonacci(1)).to.deep.equal([ 1 ]);
+  expect(fibonacci(2)).to.deep.equal([ 1, 2 ]);
+  expect(fibonacci(5)).to.deep.equal([1, 2, 3, 5 ]);
+  // ...ad nauseum.
+}); // END test(fibonacci)
+
+// Hey, where's `fibonacci`...? Uh oh...
+
+test('just even numbers?', function(){
+  expect(evens([ ]).to.deep.equal([ ]);
+  expect(evens([ 1 ]).to.deep.equal([ ]);
+  expect(evens([ 1, 1, 1 ]).to.deep.equal([ ]);
+  expect(evens([ 2 ]).to.deep.equal([ 2 ]);
+  expect(evens([ 1, 2 ]).to.deep.equal([ 2 ]);
+  // More tests, perhaps?
+}); // END test(evens)
+
+test('summing lists of Number', function(){
+  assert.equal(sum([ ]), 0);
+  assert.equal(sum([ 1 ]), 1);
+  assert.equal(sum([ 1, 1 ]), 2);
+  // ...et cetera, et cetera, et cetera.
+}); // END test(sum)
+
+// You expect me to write `sum`, too?
+
+test('summing even Fibonacci numbers?', function(){ 
+  expect( sum(evens(fibonacci(0))) ).to.equal(FILL_ME_IN); // nice.
+  // There really should be more code in here...
+}); // END test(dat CHAIN tho)
