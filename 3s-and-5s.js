033f3ce92063d80a07acee0701f2ceacd29fc2cd
Jeremy Rist <jtrain18@brandeis.edu>
2015-09-06 16:52:29 -0400

    Update 3s-and-5s.js

--- 3s-and-5s.js
+++ 3s-and-5s.js
@@ -1,1 +1,8 @@
 
+var test = require('mocha').it,
+    expect = require('chai').expect, // Use `expect(X).to.be` et al
+    assert = require('chai').assert;  // OR use `assert.equal` etc
+
+test('getting a list of multiples of 3', function(){
+  // Now what should go in here?
+}); // END test 3s
