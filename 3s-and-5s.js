1a0a8ebe4e2d2d9f9e9d50f34ab5ae74eaf5c3a2
JEREMY RIST <JTRAIN18@BRANDEIS.EDU>
2015-09-08 14:50:03 -0400

    Changes for 3s and 5s

--- 3s-and-5s.js
+++ 3s-and-5s.js
@@ -1,1 +1,17 @@
+var test = require('mocha').it,
+    expect = require('chai').expect, // Use `expect(X).to.be` et al
+    assert = require('chai').assert;  // OR use `assert.equal` etc
 
+test('getting a list of multiples of 3', function(){
+  var sum = 0;
+  for (var i = 0; i < 1000; i++){
+    if (i % 3 === 0 || i % 5 === 0){
+      sum += i;
+    }
+  }
+      return sum;
+}); // END test 3s
+
+
+
+console.assert('Sum: %d');
