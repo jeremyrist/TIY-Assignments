af150dcf58b39da926f9f7f596a8856eb5f354c4
JEREMY RIST <JTRAIN18@BRANDEIS.EDU>
2015-09-15 10:33:51 -0400

    finishing the HW from 9/14

--- TodoMVC/js/app.js
+++ TodoMVC/js/app.js
@@ -4,3 +4,12 @@
 	// Your starting point. Enjoy the ride!
 
 })(window);
+
+// User Stories
+// I can add a task to my todo list so that I can remember to do it later.
+// I can edit a task on my todo list so that I can correct typos.
+// I can mark a task as complete so that I remember that I have done it.
+// I can mark a completed task as incomplete so that I actually complete it this time.
+// I can delete a task from my todo list so that I don't have to see it any more.
+// I can delete all the completed tasks from my todo list so that I can clean up completed tasks.
+// I can filter the list of tasks so that I can see just what tasks I have completed and what are left to do.
