4b826516c0ef2c020af9806d75be0f70dd01b0f6
JEREMY RIST <JTRAIN18@BRANDEIS.EDU>
2015-09-28 00:03:38 -0400

    change the js to jquery in pseudo not practice

--- NestedAccordion/js/main.js
+++ NestedAccordion/js/main.js
@@ -3,3 +3,19 @@
 // The interactive elements take the form of clicking on headers and having them drop down paragraphs
 
 // Clicking on the same header should also roll the paragraphs back up
+
+// If the classes of the headers are "headers", then jQuery would look something like this?
+
+$(document).ready(function(){
+    $("button").click(function(){
+        $(".headers").hide();
+    });
+});
+
+// Or maybe I didn't need to add classes and could have just done:
+
+$(document).ready(function(){
+    $("button").click(function(){
+        $("h2").hide();
+    });
+});
--- ViewModeSwitch/js/scripts.js
+++ ViewModeSwitch/js/scripts.js
@@ -24,50 +24,52 @@
 
 console.log("heyo");
 
-// var page = window.location.href; // Identifies the page that you're on
-// // I think we need to define gridView but I'm not sure how to do that...
-// var switcher = document.getElementById('cbp-vm');
-//
-// document.addEventListener("click", function(){ // Adds an event listener method and assigns it the click event
-//     document.removeEventListener( "click", listView.callee, false); // I think callee is helpful here because it tells the function to pay attention
-//     switch(page){
-//       case gridView:
-//         document.getElementByClassName('cbp-vm-icon cbp-vm-grid').className += ' cbp-vm-selected' // This adds another className to the gridView
-//         break;
-//       case lis= tView:
-//         document.getElementByClassName('cbp-vm-icon cbp-vm-grid').className += ' cbp-vm-selected' // This adds another className to the listView
-//         break;
-//     }
-// });
+var page = window.location.href; // Identifies the page that you're on
+// I think we need to define gridView but I'm not sure how to do that...
+var switcher = document.getElementById('cbp-vm');
 
-var buttonGridView = document.querySelector('.cbp-vm-grid');
-var buttonListView = document.querySelector('.cbp-vm-list');
-
-console.log(buttonGridView);
-  buttonGridView.addEventListener('click', function() {
-      console.log("TRACER BULLET");}
-);
-
-console.log(buttonListView);
-  buttonListView.addEventListener('click', function() {
-      console.log("TRACER BULLET");
-
+document.addEventListener("click", function(){ // Adds an event listener method and assigns it the click event
+    document.removeEventListener( "click", listView.callee, false); // I think callee is helpful here because it tells the function to pay attention
+    switch(page){
+      case gridView:
+        document.getElementByClassName('cbp-vm-icon cbp-vm-grid').className += ' cbp-vm-selected' // This adds another className to the gridView
+        break;
+      case listView:
+        document.getElementByClassName('cbp-vm-icon cbp-vm-grid').className += ' cbp-vm-selected' // This adds another className to the listView
+        break;
+    }
 });
 
-// var switcher = document.getElementById('cbp-vm');
+// If this is correct it looks like jQuery simplifies by shortening the "document.elementInstructions" part to just a $
+$(".cbp-vm-icon cbp-vm-grid").on('click', function());
+
+// var buttonGridView = document.querySelector('.cbp-vm-grid');
+// var buttonListView = document.querySelector('.cbp-vm-list');
 //
-// var grid = switcher.firstChild;
-// var list = switcher.lastChild;
+// console.log(buttonGridView);
+//   buttonGridView.addEventListener('click', function() {
+//       console.log("TRACER BULLET");}
+// );
 //
-// console.log(grid);
-// console.log(list);
-//
-// list.addEventListener('click', function(){
-//
-// grid.className = 'cbp-vm-icon cbp-vm-grid cbp-vm-selected';
-// list.className = 'cbp-vm-icon cbp-vm-list';
-//
-//  console.log(grid);
-//  console.log(list);
-//
+// console.log(buttonListView);
+//   buttonListView.addEventListener('click', function() {
+//       console.log("TRACER BULLET");
 // });
+
+var switcher = document.getElementById('cbp-vm');
+
+var grid = switcher.firstChild;
+var list = switcher.lastChild;
+
+console.log(grid);
+console.log(list);
+
+list.addEventListener('click', function(){
+
+grid.className = 'cbp-vm-icon cbp-vm-grid cbp-vm-selected';
+list.className = 'cbp-vm-icon cbp-vm-list';
+
+ console.log(grid);
+ console.log(list);
+
+});
